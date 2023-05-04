9d09177a2f10f674a6ba8d02d1a1da1883e89a56
luci <luci@debian.debian.org>
2023-05-03 20:48:45 -0400

    .

--- app2/src/App.js
+++ app2/src/App.js
@@ -3,18 +3,17 @@ import './App.css';
 //import Evento from './src/components/Evento';
 //import Formulario from './src/components/Formulario';
 //wimport Form from './src/components/Form';
-import Condicional from './src/components/Condicional'
-import Listas from './src/components/Listas';
+//import Condicional from './src/components/Condicional'
+//import Listas from './src/components/Listas';
+import StateLift from './src/components/StateLift';
 
 function App() {
 
-  const meusItens = ['react', 'view', 'angular']
+  //const meusItens = ['react', 'view', 'angular']
 
   return (
     <div className="App">
-      <h1>Rendenizacao condicional</h1>
-      <Condicional/>
-      <Listas itens={meusItens}/>
+      
     </div>
   );
 }
